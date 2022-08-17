ff1623c7b764ef62855ba44ab4be45ee9d5a72a8
sourav dey <55862302+svdey@users.noreply.github.com>
2022-08-17 15:29:20 +0000

    21 Berg Two Soeted List

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
-#include "./aug/392IsSubsequence.hpp"
+// #include "./customHeaders/LinkedList.hpp"
+//    import class file
+#include "./aug/205IsomorphicStrings.hpp"
 #include <iostream>
-using namespace std;
 
 int main() {
   Solution *obj = new Solution();
 
   // String 
-  cout << obj->isSubsequence("acb","ahbgdc");
+  cout << obj->isIsomorphic("acb","ahbgdc");
   
   // Single output
   // cout << obj->pivotIndex({1, 2, 4, 3});
@@ -14,5 +15,22 @@ int main() {
   // vector output
   // for (auto a : obj->runningSum({1, 2, 3, 4}))
   //   cout << a << ',';
+
+
+  
+  // Linked List
+  // ListNode* list1 = new ListNode(1);
+  // list1->next = new ListNode(2);
+  // list1->next->next = new ListNode(4);
+  
+  // ListNode* list2 = new ListNode(1);
+  // list2->next = new ListNode(3);
+  // list2->next->next = new ListNode(4);
+
+  // ListNode* list = obj->mergeTwoLists(list1,list2);
+  // while(list != nullptr){
+  //   cout << list->val<<' ';
+  //   list = list->next;
+  // }
   return 0;
 }
